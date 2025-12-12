f61978c24a9f2cf049402f2d9f98fac53f6c51c9
Pedro Gomes <pedro.gomes@casaorange.com.br>
2025-12-12 17:24:50 -0300

    inicio do codigo

---
+++ src/matriz.py
@@ -0,0 +1,15 @@
+
+
+
+def Matriz():
+    
+    n_empreendimentos = 8
+    n_meses = 80
+        
+    for l in range(n_meses) :
+        for c in range(n_empreendimentos):
+            print('[]', end='')
+        print('\n')
+        
+    
+        
